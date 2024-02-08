fd76a619d035df15bd6843fe9061eaa6ce63c995
Gulshan-kumar <121786265+Gulshanray@users.noreply.github.com>
2024-02-08 15:06:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(express.urlencoded({extended:false}))
 
 app.use(express.json());
-app.use("", urlRoute)
+app.use("/url", urlRoute)
 const path = require("path")
 
 app.set('view engine', 'ejs');
